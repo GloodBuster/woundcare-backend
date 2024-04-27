6041a989b285bde4b1dfd2423f64a41fa4d6b453
José Vielma <josevglod@gmail.com>
2024-04-26 21:52:17 -0400

    setup validation pipe and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('WoundCare API')
+    .setDescription('API for WoundCare application')
+    .setVersion('1.0')
+    .addTag('WoundCare')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
