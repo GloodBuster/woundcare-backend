b75afb88ef6c221e10c61da61da8b8cf4bb24948
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2024-06-01 00:05:44 -0400

    new auth method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
 
   async login(nationalId: string, password: string): Promise<User | null> {
     const user = await this.usersService.findOne(nationalId);
@@ -21,5 +25,16 @@ export class AuthService {
     }
 
     return user;
+  }
+
+  async verifyToken(token : string) {
+    const payload = await this.jwtService.verifyAsync(token)
+    const user = await this.usersService.findOne(payload.nationalId)
+    if(!user){
+      return null
+    }
+    const {password, ...result} = user
+    
+    return result
   }
 }
