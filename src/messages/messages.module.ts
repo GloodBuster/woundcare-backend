cc299ae4a0e43a5329b691b69f91dfd1c618ae14
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2024-06-01 00:01:14 -0400

    new imports and exports on modules

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
   imports: [PrismaModule],
   controllers: [MessagesController],
   providers: [MessagesService],
+  exports: [MessagesService],
 })
 export class MessagesModule {}
