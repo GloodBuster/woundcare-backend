6dd1d98326920c4249bf2e5bfa39d15d624218d4
LuisGonzalez <luisgonzalezsalazar03@gmail.com>
2024-06-01 00:08:32 -0400

    mejor tenerlo y no necesitarlo que necesitarlo y no tenerlo

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,1 +1,19 @@
-export class CreateMessageDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateMessageDto {
+    @IsInt()
+    @IsOptional()
+    conversationId: number
+
+    @IsString()
+    @IsNotEmpty()
+    userId: string
+
+    @IsString()
+    @IsOptional()
+    text: string
+
+    @IsString()
+    @IsOptional()
+    image: string
+}
